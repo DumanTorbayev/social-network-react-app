fb58711b4c19132aa84008fd119d0ae673af850d
DumanTorbayev <dumanchikt@gmail.com>
2020-05-30 20:23:31 +0600

    class components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,9 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/no-avatar.png'
 
 
-const Users = (props) => {
+/*const Users = (props) => {
 
-    if(props.users.length === 0) {
+    if (props.users.length === 0) {
 
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
@@ -53,6 +53,59 @@ const Users = (props) => {
             }
         </div>
     )
-};
+};*/
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <div className={css.users_wrap}>
+                {
+                    this.props.users.map(u => <div className={css.users__media} key={u.id}>
+                        <div className={css.users__left}>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt=""/>
+                            {
+                                u.followed
+                                    ? <button onClick={() => {
+                                        this.props.usersUnFollow(u.id)
+                                    }}>un follow</button>
+                                    : <button onClick={() => {
+                                        this.props.usersFollow(u.id)
+                                    }}>follow</button>
+                            }
+
+                        </div>
+                        <div className={css.users__right}>
+                            <div className={css.users__userInfo}>
+                                <span className={css.users__name}>
+                                    {u.name}
+                                </span>
+                                <span>
+                                    {u.status}
+                                </span>
+                            </div>
+                            <div className={css.users__location}>
+                                <span>
+                                    {'u.location.country'}
+                                </span>
+                                <span>
+                                    {'u.location.city'}
+                                </span>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+        )
+    }
+}
 
 export default Users;
